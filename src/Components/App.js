4b9b4f4974b5bf2786ef332be87b2ecc0d1c182e
Khanya Keswa <khanya.keswa@gmail.com>
2022-12-06 12:17:49 -0500

    app pages routed

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,14 @@
-
 import React, {useEffect} from "react";
+import {Routes, Route, useNavigate, useLocation} from "react-router-dom"
 // import logo from '../Assets/Logos/logo.svg';
 import '../Stylesheets/App.css';
+import GardenerLogin from "./GardenerLogin"
+import SeasonsCarousel from "./SeasonsCarousel"
+import Garden from "./SeasonsCarousel"
+import ProduceContainer from "./ProduceContainer"
+import HarvestContainer from "./HarvestContainer"
+
+
 
 const firstEndPoint = "http://localhost:9292/"
 
@@ -18,6 +25,34 @@ function App() {
     <div>
       <header>
       </header>
+      <Routes>
+        <Route 
+        path='/login'
+        element={
+          <GardenerLogin/>
+        }>
+        </Route>
+        <Route 
+        path='/season'
+        element={
+          <SeasonsCarousel/>
+        }>
+        </Route>
+        <Route 
+        path='/produce'
+        element={
+          <ProduceContainer/>
+        }>
+        </Route>
+        <Route 
+        path='/garden'
+        element={
+          <Garden/>
+        }>
+          <Route path='/garden/harvest' element={<HarvestContainer/>}/>
+        </Route>
+      </Routes>
+      <footer></footer>
     </div>
   );
 }
--- src/Components/HarvestContainer.js
+++ src/Components/HarvestContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
-const Garden = () => {
+const HarvestContainer = () => {
     const location = useLocation()
 
     return(
@@ -11,4 +11,4 @@ const Garden = () => {
     )
 }
 
-export default Garden
+export default HarvestContainer
